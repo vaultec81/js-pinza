803cd636a146dc51ce5e055f1c6c4460b5fcdf57
vaultec81 <47548474+vaultec81@users.noreply.github.com>
2020-05-18 18:09:35 -0700

    feat: more HTTP API endpoints

--- src/cli/apiEndpoints/http/resources/cluster.js
+++ src/cli/apiEndpoints/http/resources/cluster.js
@@ -1,11 +1,107 @@
 exports.myCommitment = {
     async handler(request, h) {
         const { pinza } = request.server.app;
-        const {name, r, n} = request.query
-        if(!name) {
-            return new Error("name is a required argument")
+        const { cluster, r, n } = request.payload
+        if (!cluster) {
+            return new Error("Cluster string is a required argument")
         }
-        var cluster = pinza.cluster(name)
-        return h.response(await cluster.pin.currentCommitment(r, n))
+        try {
+            var Cluster = pinza.cluster(cluster)
+            console.log(await Cluster.pin.currentCommitment(r, n))
+            return h.response(await Cluster.pin.currentCommitment(r, n))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+exports.pin = {
+    async handler(request, h) {
+        const { pinza } = request.server.app;
+        const { cluster, cid } = request.payload
+        var Cluster = pinza.cluster(cluster)
+        try {
+            await Cluster.pin.add(cid)
+            return h.response({
+                success: true
+            });
+        } catch (err) {
+            return h.response({
+                success: false
+            })
+        }
+    }
+}
+exports.unpin = {
+    async handler(request, h) {
+        const { pinza } = request.server.app;
+        const { cluster, cid } = request.payload
+        var Cluster = pinza.cluster(cluster)
+        await Cluster.pin.rm(cid)
+        return h.response({});
+    }
+}
+exports.join = {
+    async handler(request, h) {
+        const { pinza } = request.server.app;
+        const { cluster } = request.query
+        return h.response({});
+    }
+}
+exports.leave = {
+    async handler(request, h) {
+        const { pinza } = request.server.app;
+        const { cluster } = request.query
+        return h.response({});
+    }
+}
+exports.create = {
+    async handler(request, h) {
+        const { pinza } = request.server.app;
+        const { name, options } = request.payload;
+        try {
+            await pinza.createCluster(name, options)
+        } catch (err) {
+            console.log(err)
+            if (err.code) {
+                return h.response({
+                    success: false,
+                    err: err.toString(),
+                    code: err.code
+                })
+            } else {
+                return h.response({
+                    success: false,
+                    err: err.toString()
+                })
+            }
+        }
+        return h.response({
+            success: true,
+            payload: pinza.config.get(`clusters.${name}`)
+        })
+    }
+}
+exports.export = {
+    async handler(request, h) {
+        const { pinza } = request.server.app;
+        const { cluster } = request.payload;
+        if (!cluster) {
+            cluster = pinza.config.get("defaultCluster")
+        }
+        try {
+            return h.response({
+                success: true,
+                payload: await pinza.cluster(cluster).export()
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+exports.ls = {
+    async handler(request, h) {
+        const { pinza } = request.server.app;
+        const { cluster } = request.payload;
+
     }
 }
--- src/cli/apiEndpoints/http/routes/cluster.js
+++ src/cli/apiEndpoints/http/routes/cluster.js
@@ -1,9 +1,33 @@
 const resources = require('../resources')
 module.exports = [
     {
-        method: 'GET',
+        method: 'POST',
         path: '/api/v0/cluster/mycommitment',
         options: {},
         handler: resources.cluster.myCommitment.handler
+    },
+    {
+        method: 'POST',
+        path: '/api/v0/cluster/create',
+        options: {},
+        handler: resources.cluster.create.handler
+    },
+    {
+        method: 'POST',
+        path: '/api/v0/cluster/pin/add',
+        options: {},
+        handler: resources.cluster.pin.handler
+    },
+    {
+        method: 'POST',
+        path: '/api/v0/cluster/pin/rm',
+        options: {},
+        handler: resources.cluster.unpin.handler
+    },
+    {
+        method: 'POST',
+        path: '/api/v0/cluster/export',
+        options: {},
+        handler: resources.cluster.export.handler
     }
 ]
